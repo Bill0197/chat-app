d04a077686e8142c25eb3cb67095ecfdff5da2a5
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2021-06-02 11:51:02 +0500

    initialised chat & join components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Chat from './components/Chat'
+import Join from './components/Join'
 
 export default function App() {
   return (
-    <div>
-      <h1>app</h1>
-    </div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" exact component={Chat} />
+    </Router>
   )
 }
