cc61ad0a2377677236af4897f83dbb08ed36466b
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2021-06-02 22:25:19 +0500

    modified

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,6 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'querystring'
+import io from 'socket.io-client'
 
-export default function Chat() {
+export default function Chat({ location }) {
+  useEffect(() => {
+    const { room, name } = queryString.parse(location.search)
+    console.log(room, name);
+    console.log(location.search, 'location.search')
+  }, [])
+
   return (
     <div>
       <h1>Chat</h1>
